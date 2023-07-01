c2e6196ed39a902810d17f5c61e16c292e22ada4
Sabbir Rahman <121510193+iconsabbir@users.noreply.github.com>
2023-07-01 22:30:40 +0600

    Add files via upload
    oky

---
+++ index.js
@@ -0,0 +1,37 @@
+// While Loop
+
+// var learningCoding = 0;
+// while(learningCoding < 6){
+//     console.log("I am still Learning Coding");
+//     learningCoding++;
+// }
+
+
+// var learningCoding = 0;
+// while(learningCoding < 12){
+//     console.log("I am still Learning Coding");
+//     learningCoding++;
+//     console.log(learningCoding);
+// }
+
+
+// var number = 1;
+// while(number < 30){
+//     console.log(number);
+//     number++;
+// }
+
+
+// var number = 1;
+// while(number <30){
+//     console.log(number);
+//     number = number + 2;
+// }
+
+
+// For Loop
+
+
+// for(number = 1;number <30;number = number + 2){
+//     console.log(number)
+// }
